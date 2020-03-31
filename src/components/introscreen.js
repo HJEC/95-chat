040404f552f15313bae675a6712e258c5fd67337
pizza <HJEC92@gmail.com>
2020-03-31 21:52:44 +0200

    adding svg's for intro to component

--- src/components/introscreen.js
+++ src/components/introscreen.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Intro() {
-    return <div className="intro-wrapper"></div>;
+    return (
+        <div className="intro-wrapper">
+            <img
+                src="/svgs/intro/chat_black.svg"
+                alt="logo sublayer"
+                className="chat_black"
+            />
+        </div>
+    );
 }
